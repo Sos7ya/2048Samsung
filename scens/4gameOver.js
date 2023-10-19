8c9ac62d79773ded99985eb46b95bdf3267ce811
Konstantin <k.moskal9901@gmail.com>
2023-10-19 15:26:17 +0300

    fixed gameId

--- scens/4gameOver.js
+++ scens/4gameOver.js
@@ -10,7 +10,7 @@ class GameOver extends Phaser.Scene{
         let gameOver = {
             action: 'gameOver',
             allGameSessionId : sessionID,
-            gameSessionId : gameId,
+            gameSessionId : startGame.gameSessionId,
             score : gameState.score,
             timeStamp : Date.now()
         }
